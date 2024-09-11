21f47391a619da9a6cb053056994a460c36755f5
dungdtsieucapvipro <149314686+dungdtsieucapvipro@users.noreply.github.com>
2024-09-11 20:36:06 +0700

    title & images

---
+++ test_5th.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://imgflip.com/memetemplates?page=2')
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Tìm tất cả các thẻ <h3> và <img> có class "shadow"
+    titles = soup.find_all("h3")
+    images = soup.find_all("img", class_="shadow")
+    
+    # Duyệt qua danh sách tiêu đề và hình ảnh
+    for title, image in zip(titles, images):
+        image_src = image['src'].replace("/4","",1)
+        print(f"{title.get_text(strip=True)}: https:{image_src}")
+else:
+    print(f"Failed to retrieve the page. Status code: {response.status_code}")
